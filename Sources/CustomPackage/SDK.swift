23294dd07dffefee92e5c2bf88af30a9718551a5
Ning Xu <ning_xu2@apple.com>
2023-11-17 15:52:43 -0800

    test

---
+++ Sources/CustomPackage/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ning Xu on 11/17/23.
+//
+
+import Foundation
+
+
+public struct SDK {
+    public static func hello() {
+        print("do some work")
+    }
+}
